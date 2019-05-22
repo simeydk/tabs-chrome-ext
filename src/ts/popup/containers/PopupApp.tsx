324ea3eba3c19aae5c051c4b66c5a2f8c8027219
Simey de Klerk <simeydeklerk@gmail.com>
2019-05-22 06:57:17 +0200

    simpify popup

--- src/ts/popup/containers/PopupApp.tsx
+++ src/ts/popup/containers/PopupApp.tsx
@@ -1,33 +1,5 @@
-<<<<<<< Updated upstream
-import * as React from 'react';
-import { connect } from 'react-redux';
-import { Dispatch } from 'redux';
-import styled, { ThemeProvider } from 'styled-components';
-import { IAppState } from '../../background/store';
-import Counter from '../../containers/Counter';
-import GlobalStyle from '../../components/styles/GlobalStyle';
-import { themes, ThemeTypes } from '../../components/styles/themes';
-=======
 import React, {useState} from 'react';
->>>>>>> Stashed changes
 
-
-<<<<<<< Updated upstream
-class PopupApp extends React.Component<IPopupApp> {
-
-	render() {
-		return (
-			<ThemeProvider theme={themes[this.props.theme]}>
-				<React.Fragment>
-					<GlobalStyle />
-					<PopupAppContainer>
-						<Counter />
-					</PopupAppContainer>
-				</React.Fragment>
-			</ThemeProvider>
-		);
-	}
-=======
 function App() {
 	const [i, setI] = useState(4)
 	const plus = () => setI(i+1)
@@ -40,32 +12,9 @@ function App() {
 		<button onClick={minus}>minus</button>
 		{d.toTimeString()}
 		</div>
->>>>>>> Stashed changes
 }
 
-const mapStateToProps = (state: IAppState) => {
-	return {
-		theme: state.settings.theme
-	};
-};
 
-<<<<<<< Updated upstream
-export default connect(mapStateToProps)(PopupApp);
-
-const PopupAppContainer = styled('div')`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    justify-items: center;
-    align-items: center;  
-    height: 200px;
-    width: 300px;
-    margin: 10px;
-    background-color: ${p => p.theme.backgroundColor};
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-`;
-=======
 
 // export default connect(mapStateToProps)(PopupApp);
 export default App;
->>>>>>> Stashed changes
